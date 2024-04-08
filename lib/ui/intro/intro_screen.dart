4f1d9b55cfc539dbbcc0861a612d2b08815f8594
Nguyen Minh Tien <nmtienptg@gmail.com>
2024-04-08 22:23:34 +0700

    intro

--- lib/ui/intro/intro_screen.dart
+++ lib/ui/intro/intro_screen.dart
@@ -22,7 +22,7 @@ class _IntroScreenState extends State<IntroScreen> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          const SizedBox(height: 30),
+          const SizedBox(height: 48),
           ImageSlideshow(
             disableUserScrolling: true,
             height: MediaQuery.of(context).size.height * 0.7,
@@ -38,9 +38,12 @@ class _IntroScreenState extends State<IntroScreen> {
             /// Called whenever the page in the center of the viewport changes.
             onPageChanged: (value) {
               if (value == 2) {
-                Future.delayed(const Duration(seconds: 2), (() {
+                Future.delayed(const Duration(milliseconds: 1500), (() {
                   setState(() {
-                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomeTabs()));
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const HomeTabs()));
                   });
                 }));
               }
@@ -92,14 +95,14 @@ class _IntroScreenState extends State<IntroScreen> {
               Column(
                 children: [
                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    padding: const EdgeInsets.symmetric(horizontal: 90),
                     child: Text(
                       'Hỗ trợ hiển thị mô hình 3D trên mặt phẳng',
                       style: OneTheme.of(context).body1.copyWith(fontSize: 28),
                       textAlign: TextAlign.center,
                     ),
                   ),
-                  const SizedBox(height: 50),
+                  const SizedBox(height: 30),
                   Image.asset(
                     ArImages.intro_3,
                     fit: BoxFit.contain,
@@ -109,15 +112,15 @@ class _IntroScreenState extends State<IntroScreen> {
               ),
             ],
           ),
-          const SizedBox(height: 25),
+          // const SizedBox(height: 15),
           Text(
-            'Xin chào',
+            'Xin chào!',
             style: OneTheme.of(context).body1.copyWith(fontSize: 28),
           ),
           Lottie.asset(
             "assets/animation/loading.json",
             width: 110,
-            height: 110,
+            height: 100,
             repeat: true,
             fit: BoxFit.contain,
           ),
